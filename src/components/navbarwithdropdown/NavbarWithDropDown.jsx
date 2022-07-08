80d01b2704f435c43960d42fb59435ba6693efc2
tjeong <k.jts8257@gmail.com>
2022-07-09 08:57:19 +0900

    sidbar

--- src/components/navbarwithdropdown/NavbarWithDropDown.jsx
+++ src/components/navbarwithdropdown/NavbarWithDropDown.jsx
@@ -31,7 +31,6 @@ function Navbar(props) {
   return (
     <nav className="navbar">
       <ul className="navbar-nav">{props.children}</ul>
-
     </nav>
   )
 }
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,37 +1,71 @@
 import React, {useState} from 'react'
-import style from './style.scss'
+import { ReactComponent as BellIcon } from '../../icons//svgs/bell.svg';
+import { ReactComponent as MessengerIcon } from '../../icons/svgs/messenger.svg';
 import { ReactComponent as CaretIcon } from '../../icons/svgs/caret.svg';
+import { ReactComponent as PlusIcon } from '../../icons//svgs/plus.svg';
+import { ReactComponent as CogIcon } from '../../icons//svgs/cog.svg';
+import { ReactComponent as ChevronIcon } from '../../icons//svgs/chevron.svg';
+import { ReactComponent as ArrowIcon } from '../../icons//svgs/arrow.svg';
+import { ReactComponent as BoltIcon } from '../../icons//svgs/bolt.svg';
+import style from './style.scss'
+import {CSSTransition} from 'react-transition-group'
 
 function SideBar() {
-  const [open, setOpen] = useState(false)
+  const [isOpen, setOpen] = useState(false)
   const [navMaginR, setNavMarginR] = useState(0)
+
+  const onNavButtonClick = () => {
+    setOpen(!isOpen)
+    if (navMaginR > 0) {
+      setNavMarginR(0)
+      console.log(navMaginR)
+    } else {
+      setNavMarginR(250)
+      console.log(navMaginR)
+    }
+  }
 
   return (
     <div>
-      <div className='navbar'>
-        <li className="nav-item">
+      <div className='nav-bar-with-side'
+        style={{marginRight:navMaginR }}>
+      <li className="nav-item">
         <div 
           className="icon-button" 
-          onClick={() => {
-            setOpen(!open)
-            setNavMarginR(300)
-          }}>
+          onClick={() => {onNavButtonClick()}}>
           {<CaretIcon/>}
         </div>
       </li>
       </div>
-      {open &&  <SidebarWrapper/>}
+      {isOpen &&  <SidebarContainer/>}
     </div>
   )
 }
 
 export default SideBar
 
-function SidebarWrapper() {
+function SidebarContainer() {
   return (
-    <div className='sidebar-container'>
-      hello
+    <div 
+      className='sidebar-container'>
+      <ul>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+        <SidebarItem icon={<BellIcon/>}str={'hello'}/>
+      </ul>
     </div>
-    
+  )
+}
+
+function SidebarItem({icon, str}) {
+  return(
+    <li className='sidebar-item'>
+      <div className='sidebar-item-left-icon'>{icon}</div>
+      <span className='sidebar-item-text'>{str}</span>
+    </li>
   )
 }
